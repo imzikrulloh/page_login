a580999407757f927ea9c70d813eb04002869ada
ZikrullohTutorials <106254977+imzikrulloh@users.noreply.github.com>
2023-01-06 12:20:11 +0300

    Add files via upload

---
+++ src/fremwork/web02/App.vue
@@ -0,0 +1,81 @@
+<template>
+<section class="header">
+  <div class="container">
+    <div class="navbar">
+      <div class="logo">
+        <img src="./assets/image%2014.png" alt="">
+      </div>
+      <nav>
+        <a href="#"><f>Home</f></a>
+        <a href="#">Aboute</a>
+        <a href="#">Shop</a>
+        <a href="#">Pricing <i class="fa fa-sort-desc"></i></a>
+        <a href="#">Pages</a>
+        <a href="#">Blogs</a>
+        <a href="#">Contact</a>
+        <a href="#"><i class="fa fa-search"></i></a>
+        <a href="#"><i class="fa fa-cart-shopping"></i></a>
+      </nav>
+    </div>
+    <div class="header-text">
+<div class="text">
+      <h1>
+        Your favourite books, <br>
+        all in one place
+      </h1><br>
+      <p>
+        A lectus ac pulvinar tincidunt accumsan ullamcorper dolor at lectus <br>
+        acsed facilisis hac molestie aliquam ut blandit.
+
+      </p>
+  <div>
+      <div class="btn">
+        <a href="#">Shop Now</a>
+      </div>
+  </div>
+</div>
+    </div>
+    <div class="header-book">
+      <div class="book1"><img src="./assets/image%2017.png" alt=""></div>
+      <div class="book2"><img src="./assets/image%2016.png" alt=""></div>
+      <div class="book3"><img src="./assets/image%2020.png" alt=""></div>
+      <div class="book4"><img src="./assets/image%2018.png" alt=""></div>
+      <div class="book5"><img src="./assets/image%2019.png" alt=""></div>
+    </div>
+  </div>
+</section>
+  <section class="header-bottom">
+    <div class="container">
+      <div class="button"><a href="#"><i class="fa fa-truck fa-1x"></i>Free Worldwide Shipping</a></div>
+      <div class="button"><a href="#"><i class="fa fa-check"></i>Refund Guarantee</a></div>
+      <div class="button"><a href="#"><i class="fa fa-compress"></i>Happy Customer</a></div>
+    </div>
+  </section>
+  <section class="book-categories">
+    <div class="container">
+      <div class="book-text">
+        <h1>Book Categories</h1>
+      </div>
+      <div class="book-categories-cards">
+        <div class="card"><img src="./assets/book-card/Ws4wd-vJ9M0.png" alt=""></div>
+        <div class="card"><img src="./assets/book-card/Ws4wd-vJ9M0.png" alt=""></div>
+        <div class="card"><img src="./assets/book-card/Ws4wd-vJ9M0.png" alt=""></div>
+        <div class="card"><img src="./assets/book-card/Ws4wd-vJ9M0.png" alt=""></div>
+        <div class="card"><img src="./assets/book-card/Ws4wd-vJ9M0.png" alt=""></div>
+        <div class="card"><img src="./assets/book-card/Ws4wd-vJ9M0.png" alt=""></div>
+      </div>
+   </div>
+  </section>
+</template>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Roboto:wght@300&display=swap" rel="stylesheet">
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
